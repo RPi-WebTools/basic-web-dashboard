5533705e9bb1144fdc7f2f3d50f1eb1cd6c257b4
Viperinius <41393831+Viperinius@users.noreply.github.com>
2020-02-27 22:43:27 +0100

    init project
    Signed-off-by: Viperinius <41393831+Viperinius@users.noreply.github.com>

---
+++ src/views/Contact.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="contact">
+        <h1>Contact</h1>
+        <p>
+            <ul>
+                <li>Mail: {{email}}</li>
+                <li>Web: {{web}}</li>
+            </ul>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Contact',
+    data() {
+        return {
+            email: 'test@test.com',
+            web: 'https://.ddg.gg'
+        }
+    }
+}
+</script>
