6aa75c1ba4eae3cfaecc9a038e8dea0c42a4dbfd
Viperinius <41393831+Viperinius@users.noreply.github.com>
2020-04-27 22:16:44 +0200

    got nav drawer working with routing

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,28 +1,8 @@
 <template>
-    <!--<md-list :style="{ backgroundImage: 'url(' + image + ')', backgroundSize: 'cover', backgroundPosition: 'center center' }">
-        <md-list-item>
-            <md-avatar>
-                <img src="@/assets/rpi-webtools.png" alt="Logo">
-            </md-avatar>
-            <span class="md-list-item-text">{{ title }}</span>
-        </md-list-item>
-        <br>
-        <md-divider></md-divider>
-        <md-list-item v-for="link in links" v-bind:key="link.id" :to="{ name: link.to }" :md-expand="link.expandable">
-            <md-icon :md-src="link.src"></md-icon>
-            <span class="md-list-item-text">{{ link.title }}</span>
-
-            <md-list v-if="link.expandable" slot="md-expand" style="background: none;">
-                <md-list-item v-for="sub in link.sub" v-bind:key="sub.id" :to="{ name: sub.to }">
-                    <md-icon :md-src="sub.src"></md-icon>
-                    <span class="md-list-item-text">{{ sub.title }}</span>
-                </md-list-item>
-            </md-list>
-        </md-list-item>
-    </md-list>-->
     <v-list rounded>
-        <v-list-group color="primary" v-for="(link, index) in links" :key="index" v-model="link.active">
-            <!--<v-list-group v-if="link.expandable">
+        <template v-for="(link, i) in links">
+            <v-divider v-if="link.divider" :key="'dividier-' + i"/>
+            <v-list-group v-if="link.expandable" :key="i" style="margin-top: 5px; margin-bottom: 5px;">
                 <v-icon slot="prependIcon" v-text="link.src" style="max-width: 24px;"></v-icon>
                 <template v-slot:activator>
                     <v-list-item-content>
@@ -30,36 +10,37 @@
                     </v-list-item-content>
                 </template>
 
-                <v-list-item v-for="(sub, subIndex) in link.sub" :key="subIndex" :to="sub.to">
+                <v-list-item
+                    v-for="(sub, subIndex) in link.sub"
+                    :key="subIndex"
+                    :to="{ name: sub.to }"
+                    router
+                    exact
+                >
                     <v-list-item-icon>
                         <v-icon v-text="sub.src" style="max-width: 24px;"></v-icon>
                     </v-list-item-icon>
                     <v-list-item-content>
-                        <v-list-item-title v-text="sub.title"></v-list-item-title>
+                        <v-list-item-title v-text="sub.title" />
                     </v-list-item-content>
                 </v-list-item>
             </v-list-group>
-
-            <v-list-group v-else append-icon="">
-                <v-icon slot="prependIcon" v-text="link.src" style="max-width: 24px;"></v-icon>
-                <template v-slot:activator>
-                    <v-list-item-content>
-                        <v-list-item-title v-text="link.title"></v-list-item-title>
-                    </v-list-item-content>
-                </template>
-            </v-list-group>-->
-            <v-list-tile slot="link" :to="link.to">
-                <v-list-tile-action>
+            <v-list-item
+                v-else
+                :key="i"
+                :to="{ name: link.to }"
+                router
+                exact
+                style="margin-top: 5px;"
+            >
+                <v-list-item-icon>
                     <v-icon v-text="link.src" style="max-width: 24px;"></v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content>
-                    <v-list-tile-title >{{ link.title }}</v-list-tile-title>
-                </v-list-tile-content>
-                <v-list-tile-action v-if="link.sub.length > 0">
-                    <v-icon>keyboard_arrow_down</v-icon>
-                </v-list-tile-action>
-            </v-list-tile>
-        </v-list-group>
+                </v-list-item-icon>
+                <v-list-item-content>
+                    <v-list-item-title v-text="link.title" />
+                </v-list-item-content>
+            </v-list-item>
+        </template>
     </v-list>
 </template>
 
@@ -72,35 +53,31 @@ export default {
                 {
                     title: 'Home',
                     to: 'Home',
-                    // src: require('../assets/home-solid.svg'),
                     src: 'fas fa-home',
                     expandable: false,
                     sub: []
                 },
                 {
                     title: 'Dashboard',
-                    // src: require('../assets/chart-pie-solid.svg'),
                     src: 'fas fa-chart-pie',
                     expandable: true,
+                    divider: true,
                     sub: [
                         {
                             title: 'CPU & Memory',
                             to: 'CpuAndMemory',
-                            // src: require('../assets/microchip-solid.svg'),
                             src: 'fas fa-microchip',
                             expandable: false
                         },
                         {
                             title: 'Network',
                             to: 'Network',
-                            // src: require('../assets/network-wired-solid.svg'),
                             src: 'fas fa-network-wired',
                             expandable: false
                         },
                         {
                             title: 'Storage',
                             to: 'Filesystem',
-                            // src: require('../assets/hdd-solid.svg'),
                             src: 'fas fa-hdd',
                             expandable: false
                         }
@@ -108,21 +85,18 @@ export default {
                 },
                 {
                     title: 'Docker',
-                    // src: require('../assets/docker-brands.svg'),
                     src: 'fab fa-docker',
                     expandable: true,
                     sub: [
                         {
                             title: 'Manage',
                             to: 'DockerManage',
-                            // src: require('../assets/tasks-solid.svg'),
                             src: 'fas fa-tasks',
                             expandable: false
                         },
                         {
                             title: 'Baïkal',
                             to: 'Baikal',
-                            // src: require('../assets/calendar-alt-solid.svg'),
                             src: 'fas fa-calendar-alt',
                             expandable: false
                         }
@@ -130,21 +104,18 @@ export default {
                 },
                 {
                     title: 'Gitea',
-                    // src: require('../assets/git-square-brands.svg'),
                     src: 'fab fa-git-square',
                     expandable: true,
                     sub: [
                         {
                             title: 'Statistics',
                             to: 'GiteaStatistics',
-                            // src: require('../assets/clipboard-list-solid.svg'),
                             src: 'fas fa-clipboard-list',
                             expandable: false
                         },
                         {
                             title: 'Configuration',
                             to: 'GiteaConfig',
-                            // src: require('../assets/cogs-solid.svg'),
                             src: 'fas fa-cogs',
                             expandable: false
                         }
@@ -153,28 +124,24 @@ export default {
                 {
                     title: 'System Services',
                     to: 'SysServ',
-                    // src: require('../assets/server-solid.svg'),
                     src: 'fas fa-server',
                     expandable: false,
                     sub: []
                 },
                 {
                     title: 'TVspotter',
-                    // src: require('../assets/tmdb-logo-square.svg'),
                     src: '$vuetify.icons.tmdbLogoSquare',
                     expandable: true,
                     sub: [
                         {
                             title: 'Movies',
                             to: 'Movies',
-                            // src: require('../assets/film-solid.svg'),
                             src: 'fas fa-film',
                             expandable: false
                         },
                         {
                             title: 'TV shows',
                             to: 'TV',
-                            // src: require('../assets/tv-solid.svg'),
                             src: 'fas fa-tv',
                             expandable: false
                         }
@@ -183,7 +150,6 @@ export default {
                 {
                     title: 'About',
                     to: 'About',
-                    // src: require('../assets/flag-solid.svg'),
                     src: 'fas fa-flag',
                     expandable: false,
                     divider: true,
--- src/views/BaseLayout.vue
+++ src/views/BaseLayout.vue
@@ -1,6 +1,22 @@
 <template>
-    <div id="layout">
-        <v-navigation-drawer app clipped :src="require('../assets/sidebarBG.png')" v-model="state">
+    <div>
+        <v-app-bar app light color="primary" dense clipped-left>
+            <v-app-bar-nav-icon @click.stop="navState = !navState">
+            </v-app-bar-nav-icon>
+
+            <v-img alt="Logo" class="mx-2 hidden-sm-and-down" contain max-width="40" max-height="40" src="@/assets/rpi-webtools.png"/>
+            <!--<v-img alt="Dashboard" class="mx-2 hidden-sm-and-down" contain max-height="25" src="@/assets/rpi-dashboard.png"/>-->
+            <v-toolbar-title class="ml-2 hidden-sm-and-down">RPi Dashboard</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn text>
+                <span class="mr-2">Sign in (coming later)</span>
+                <v-icon>fas fa-sign-in-alt</v-icon>
+            </v-btn>
+        </v-app-bar>
+
+        <v-navigation-drawer app clipped :src="require('../assets/sidebarBG.png')" v-model="navState">
             <sidebar/>
         </v-navigation-drawer>
 
@@ -21,17 +37,9 @@ export default {
     components: {
         Sidebar
     },
-    props: [
-        'navState'
-    ],
     data: () => {
         return {
-            state: true
-        }
-    },
-    watch: {
-        navState () {
-            this.state = this.navState
+            navState: true
         }
     }
 }
