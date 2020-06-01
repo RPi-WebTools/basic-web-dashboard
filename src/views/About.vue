44a1d1a8da3d2b6ef76a89651fd80afe8dd0dc25
Viperinius <41393831+Viperinius@users.noreply.github.com>
2020-06-01 18:00:43 +0200

    filled About page with some info

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,10 +3,59 @@
         <v-row>
             <v-col cols="12">
                 <v-card class="px-2 pb-3 pt-1" tile>
-                    <p>There should be data coming your way...watch out!</p>
-                    <p>{{ testData }}</p>
-
-                    <p>This product uses the TMDb API but is not endorsed or certified by TMDb.</p>
+                    <p class="headline pl-3 pt-3">About</p>
+                    <v-container fluid>
+                        <v-row>
+                            <v-col cols="12">
+                                <p>Made by RPi-WebTools 2020, maintained by Viperinius.</p>
+                                <p>
+                                    This project can be found
+                                    <v-btn text small color="primary" class="ma-0 pa-0" href="https://github.com/RPi-WebTools/basic-web-dashboard" target="_blank">
+                                        on Github
+                                    </v-btn>
+                                    . Backend is handled by an Express.js server, which is also versioned
+                                    <v-btn text small color="primary" class="ma-0 pa-0" href="https://github.com/RPi-WebTools/express-server" target="_blank">
+                                        on Github
+                                    </v-btn>
+                                    .
+                                </p>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col cols="12">
+                                <p></p>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                </v-card>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="12">
+                <v-card class="px-2 pb-3 pt-1" tile>
+                    <p class="headline pl-3 pt-3">License stuff</p>
+                    <v-container fluid>
+                        <v-row>
+                            <v-col cols="12">
+                                <p>
+                                    This project uses FontAwesome icons.
+                                    <v-btn text small color="primary" class="ml-1" href="https://fontawesome.com/license" target="_blank">
+                                        License
+                                    </v-btn>
+                                </p>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col cols="12">
+                                <p>
+                                    This product uses the TMDb API but is not endorsed or certified by TMDb.
+                                    <v-btn text small color="primary" class="ml-1" href="https://www.themoviedb.org/" target="_blank">
+                                        Link
+                                    </v-btn>
+                                </p>
+                            </v-col>
+                        </v-row>
+                    </v-container>
                 </v-card>
             </v-col>
         </v-row>
@@ -14,15 +63,7 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
-
 export default {
-    name: 'About',
-    mounted () {
-        this.$store.dispatch('SYSSERV/TESTTHING/GET_TEST_DATA')
-    },
-    computed: {
-        ...mapState('SYSSERV/TESTTHING', ['testData'])
-    }
+    name: 'About'
 }
 </script>
